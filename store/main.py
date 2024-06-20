ee2d68ab6da96ffb75b940fa76d36e121e6eb0e6
luiz315 <luizvictorreis315@gmail.com>
2024-06-20 19:40:53 -0300

    feat: add configs

--- store/main.py
+++ store/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
 from store.core.config import settings
-
-# from store.routers import api_router
+from store.routers import api_router
 
 
 class App(FastAPI):
@@ -17,4 +16,4 @@ class App(FastAPI):
 
 
 app = App()
-# app.include_router(api_router)
+app.include_router(api_router)
